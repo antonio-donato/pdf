4f7b74bba27a1fa73ecbf6ec7b984d2d3f978346
Antonio Donato <antonio.donato@cgm.com>
2019-08-27 12:29:47 +0200

    merge of all pdf file in one

---
+++ pdf2.py
@@ -0,0 +1,22 @@
+import PyPDF2
+from os import listdir
+import os
+
+# file in folder
+listaFile = listdir()
+
+#open write pdf merger file
+nomeFileDestinazione = input()
+PDF_Destinazione = open(nomeFileDestinazione, "wb")
+merger = PyPDF2.PdfFileMerger()
+
+
+for nomeFile in listaFile:
+    if nomeFile.endswith(".pdf"):
+        pdfFile = open(nomeFile, "rb")
+        readerPDF = PyPDF2.PdfFileReader(pdfFile)
+        merger.append(readerPDF)
+        pdfFile.close()
+
+merger.write(PDF_Destinazione)
+PDF_Destinazione.close()
