2d8f2c2af00057b2745385c496e1aaeda2e8870e
Antonio Donato <antonio.donato@cgm.com>
2019-08-27 11:18:25 +0200

    prima pagina del pdf triplicata in un nuovo pdf

--- pdf.py
+++ pdf.py
@@ -1,7 +1,30 @@
 import PyPDF2
 
-pdfFile1 = open("C:\\Users\\antonio.donato\\Google Drive\\G3 Shared\\Travel\\Old\\56219783.pdf", "rb")
+pdfFile1 = open("Auto Moretti - Donato - Francoforte - 15-17 maggio 2019.pdf", "rb")
 
 reader1 = PyPDF2.PdfFileReader(pdfFile1)
 
-pdfFile2 = open("c:\\test.pdf", "wb")
+pdfFile2 = open("test.pdf", "wb")
+writer1 = PyPDF2.PdfFileWriter()
+
+pag_0 = reader1.getPage(0)
+
+writer1.addPage(pag_0)
+writer1.addPage(pag_0)
+writer1.addPage(pag_0)
+
+writer1.write(pdfFile2)
+
+numPage = reader1.numPages
+print(numPage)
+
+pdfFile1.close()
+pdfFile2.close()
+
+pdfFile2 = open("test.pdf", "rb")
+reader2 = PyPDF2.PdfFileReader(pdfFile2)
+
+numPage = reader2.numPages
+print(numPage)
+
+pdfFile2.close()
