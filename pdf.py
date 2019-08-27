d7b5c14a3649663fb8943af94e9401d228ae199f
Antonio Donato <antonio.donato@cgm.com>
2019-08-27 10:29:10 +0200

    aggiunta apertura file di output pdf

--- pdf.py
+++ pdf.py
@@ -2,3 +2,6 @@ import PyPDF2
 
 pdfFile1 = open("C:\\Users\\antonio.donato\\Google Drive\\G3 Shared\\Travel\\Old\\56219783.pdf", "rb")
 
+reader1 = PyPDF2.PdfFileReader(pdfFile1)
+
+pdfFile2 = open("c:\\test.pdf", "wb")
