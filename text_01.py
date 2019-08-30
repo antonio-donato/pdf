1cecc9be1a7085cdea209941deb4e3377fd8b092
Antonio Donato <antonio.donato@cgm.com>
2019-08-30 17:53:04 +0200

    evito i file con errori e conto elaborati ed errori

--- text_01.py
+++ text_01.py
@@ -1,14 +1,21 @@
 import os
 print("Digita il percorso della cartella da valutare:")
-cartella = input()
+# cartella = input()
 
 print()
 print("Digita la stringa da cercare:")
-stringSearch = input()
+# stringSearch = input()
 
 print()
 print("Digita la nuova stringa da sostituire:")
-stringSub = input()
+# stringSub = input()
+
+# solo per test
+cartella = "C:\\Workspace_git\\Wingesfar"
+stringSearch = "56821C"
+stringSub = "PIPPOOOO!!!"
+contatore = 0
+errori = 0
 
 #listaFile = os.listdir(cartella)
 
@@ -18,12 +25,23 @@ for nomeFile in os.listdir(cartella):
     if os.path.isfile(os.path.join(cartella, nomeFile)) and nomeFile.upper().endswith(".CBL"):
         print('file: {0}'.format(nomeFile))
 
-        fileInput = open(os.path.join(cartella, nomeFile), "rt")
-        data = fileInput.read()
-        data.replace(stringSearch, stringSub)
-        fileInput.close()
+        fileInput = open(os.path.join(cartella, nomeFile), "rt", encoding="cp1252")
 
-        #fileInput = fileInput.replace(stringSearch, stringSub)
-        fileOut = open(os.path.join(cartella, nomeFile), "wt")
-        fileOut.write(data)
-        fileOut.close()
+        try:
+            data = fileInput.read()
+            data.replace(stringSearch, stringSub)
+            fileInput.close()
+
+            fileOut = open(os.path.join(cartella, nomeFile), "wt")
+            fileOut.write(data)
+            fileOut.close()
+
+            contatore += 1
+
+        except:
+            print("Errore file su {0}".format(fileInput))
+
+            errori += 1
+
+print("Elaborati {0} File".format(contatore))
+print("Errori {0}".format(errori))
