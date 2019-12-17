58205e868ccfdcbfc3b0d24f0dff61af3b2f697a
Antonio Donato <antonio.donato@cgm.com>
2019-12-17 18:04:55 +0100

    Rimossi file inutili e inserita prova su getDocumentInfo

--- pdf.py
+++ pdf.py
@@ -1,6 +1,6 @@
 import PyPDF2
 
-pdfFile1 = open("Auto Moretti - Donato - Francoforte - 15-17 maggio 2019.pdf", "rb")
+pdfFile1 = open("boarding passes_Arrigoni_Donato_Ziboni_ 6-9 maggio 2019.pdf", "rb")
 
 reader1 = PyPDF2.PdfFileReader(pdfFile1)
 
@@ -8,10 +8,11 @@ pdfFile2 = open("test.pdf", "wb")
 writer1 = PyPDF2.PdfFileWriter()
 
 for numeroPagina in range(reader1.numPages):
-    writer1.addPage(reader1.getPage(numeroPagina))
+    writer1.addPage(reader1.getPage(0))
+
+print(reader1.getDocumentInfo().title)
 
 writer1.write(pdfFile2)
 
 pdfFile1.close()
 pdfFile2.close()
-
