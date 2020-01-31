a38b6f246a0740b4d6947c2ffbb9419244d71b62
Antonio Donato <antonio.donato@cgm.com>
2020-01-31 18:04:13 +0100

    Creata una finestra di selezione file

--- pdf.py
+++ pdf.py
@@ -1,16 +1,33 @@
 import PyPDF2
+import tkinter as tk
+from tkinter import filedialog
+import os
 
-pdfFile1 = open("boarding passes_Arrigoni_Donato_Ziboni_ 6-9 maggio 2019.pdf", "rb")
+root = tk.Tk()
+root.withdraw()
+
+# File PDF da aprire
+file_path = filedialog.askopenfilename()
+# devo aggiungere gli apici altrimenti non me lo apre
+file_path_complete = '"' + file_path + '"'
+
+#apertura file
+os.system(file_path_complete)
+
+pdfFile1 = open(file_path, "rb")
 
 reader1 = PyPDF2.PdfFileReader(pdfFile1)
 
-pdfFile2 = open("test.pdf", "wb")
+#chiedo dove salvare il file finale di una sola pagina
+file_path_saved = filedialog.asksaveasfilename()
+
+pdfFile2 = open(file_path_saved, "wb")
 writer1 = PyPDF2.PdfFileWriter()
 
 for numeroPagina in range(reader1.numPages):
     writer1.addPage(reader1.getPage(0))
 
-print(reader1.getDocumentInfo().title)
+# print(reader1.getDocumentInfo().title)
 
 writer1.write(pdfFile2)
 
