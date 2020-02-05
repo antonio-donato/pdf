561f1547ec2658e5ee7a20dc25984cfd88ef4556
Antonio Donato <antonio.donato@cgm.com>
2020-02-05 17:53:45 +0100

    Creo una finestra di selezione della pagina da salvare da sola

--- pdf.py
+++ pdf.py
@@ -1,6 +1,8 @@
 import PyPDF2
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import *
+from functools import partial
 import os
 
 root = tk.Tk()
@@ -17,6 +19,27 @@ os.system(file_path_complete)
 pdfFile1 = open(file_path, "rb")
 
 reader1 = PyPDF2.PdfFileReader(pdfFile1)
+numeroPages = reader1.numPages
+
+#window
+tkWindow = Tk()
+tkWindow.geometry('400x150')
+tkWindow.title('Scegli la pagina da salvare')
+
+#Label
+usernameLabel = Label(tkWindow, text='Scegli una pagina da 1 a %d' % numeroPages).grid(row=2, column=0)
+# selected = IntVar()
+selected = StringVar()
+
+usernameEntry = Entry(tkWindow, textvariable=selected).grid(row=2, column=2)
+
+def clicked():
+    print(selected.get())
+
+#ok button
+# okButton = Button(tkWindow, text="OK").grid(row=4, column=0)
+
+tkWindow.mainloop()  #TODO Perchè non va oltre?
 
 #chiedo dove salvare il file finale di una sola pagina
 file_path_saved = filedialog.asksaveasfilename()
@@ -24,12 +47,14 @@ file_path_saved = filedialog.asksaveasfilename()
 pdfFile2 = open(file_path_saved, "wb")
 writer1 = PyPDF2.PdfFileWriter()
 
-for numeroPagina in range(reader1.numPages):
-    writer1.addPage(reader1.getPage(0))
-
-# print(reader1.getDocumentInfo().title)
-
+selectedNumPage = IntVar()
+selectedNumPage = selected
+# for numeroPagina in range(reader1.numPages):
+writer1.addPage(reader1.getPage(selectedNumPage - 1))
+#
+# # print(reader1.getDocumentInfo().title)
+#
 writer1.write(pdfFile2)
-
+#
 pdfFile1.close()
 pdfFile2.close()
