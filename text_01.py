893a79cd8b49e11c870f0042d2487e4694178cc3
Antonio Donato <antonio.donato@cgm.com>
2020-07-22 10:05:07 +0200

    Cartella locale casa (d:\) Aggiunta variabile per trovati

--- text_01.py
+++ text_01.py
@@ -11,10 +11,12 @@ print("Digita la nuova stringa da sostituire:")
 # stringSub = input()
 
 # solo per test
-cartella = "C:\\Workspace_git\\Wingesfar"
+cartella = "D:\\Github_repository\\Wingesfar"
+#"C:\\Workspace_git\\Wingesfar"
 stringSearch = "56821C"
 stringSub = "Python!!!"
 contatore = 0
+trovati = 0
 errori = 0
 
 #listaFile = os.listdir(cartella)
@@ -23,7 +25,7 @@ for nomeFile in os.listdir(cartella):
     valore = os.path.isfile(os.path.join(cartella, nomeFile))
 
     if os.path.isfile(os.path.join(cartella, nomeFile)) and nomeFile.upper().endswith(".CBL"):
-        print('file: {0}'.format(nomeFile))
+        # print('file: {0}'.format(nomeFile))
 
         fileInput = open(os.path.join(cartella, nomeFile), "rt", encoding="cp1252")
 
@@ -32,6 +34,7 @@ for nomeFile in os.listdir(cartella):
             data1 = data.replace(stringSearch, stringSub)
             if data != data1:
                 print(data1)
+                trovati += 1
             fileInput.close()
 
             fileOut = open(os.path.join(cartella, nomeFile), "wt")
@@ -46,4 +49,5 @@ for nomeFile in os.listdir(cartella):
             errori += 1
 
 print("Elaborati {0} File".format(contatore))
+print("Sostituiti {0} File".format(trovati))
 print("Errori {0}".format(errori))
